a96499ee830cdf4e326d9eedda55ea85726f6c03
Hyma Kantamneni <136876127+HymaKantamneni@users.noreply.github.com>
2024-10-18 18:25:26 +0530

    Add files via upload

---
+++ QR-Attendance-System-main/run.py
@@ -0,0 +1,5 @@
+from manage.py import create_app
+
+app = create_app()
+
+#postgres://qr_example_user:o88NIssKH9IzYCCdEbskyuIn28WeatUK@dpg-cops7usf7o1s73e6bcng-a.oregon-postgres.render.com/qr_example
